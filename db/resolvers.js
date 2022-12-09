1728a568fe7ce9fab383fba5cf143f9287b37c1d
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2022-12-09 18:36:54 -0500

    Creando el Mutation para nuevos Pedidos

--- db/resolvers.js
+++ db/resolvers.js
@@ -68,13 +68,13 @@ const resolvers = {
       try {
         // Destructuring
         const { email, password } = input;
-        // Check user unique
+        // Check if user is unique
         const exists = await User.findOne({ email });
         if (exists) throw new Error("The user is already registered.");
         // Hashear password
         const salt = await bcrypt.getSalt(10);
         input.password = await bcrypt.hash(password, salt);
-        // Save database
+        // Save in database
         const user = new User(input);
         user.save();
         return user;
@@ -84,7 +84,7 @@ const resolvers = {
       try {
         // Destructuring
         const { email, password } = input;
-        // Check user exists
+        // Check if the user exists
         const user = await User.findOne({ email });
         if (user) throw new Error("Username does not exist.");
         // Check password is correct
@@ -106,17 +106,17 @@ const resolvers = {
     // ========= Products =========
     createProduct: async (_, { input }) => {
       try {
-        // Save database
+        // Save in database
         const product = new Product(input);
         return await product.save();
       } catch (error) {}
     },
     updateProduct: async (_, { id, input }) => {
       try {
-        // Check product
+        // Check if the product exists
         let product = await Product.findById(id);
         if (!product) throw new Error("Product not found!");
-        // Save product update
+        // Save updated product
         return await Product.findOneAndUpdate({ _id: id }, input, {
           new: true,
         });
@@ -124,9 +124,10 @@ const resolvers = {
     },
     deleteProduct: async (_, { id }) => {
       try {
-        // Check product
+        // Check if the product exists
         let product = await Product.findById(id);
         if (!product) throw new Error("Product not found!");
+        // Deleted product
         await Product.findOneAndDelete({ _id: id });
         return "Product delete!";
       } catch (error) {}
@@ -136,27 +137,26 @@ const resolvers = {
       try {
         // Destructuring
         const { email } = input;
-        // Check client
+        // Check if the client exists
         let exits = await Client.findOne({ email });
         if (!exits) throw new Error("The client is already registered.");
-
         // Create instance client
         const client = new Client(input);
-        // Assign vendor
+        // Assign seller
         client.vendor = ctx.user.id;
-        // Save database
+        // Save in database
         return await client.save();
       } catch (error) {}
     },
     updateClient: async (_, { id, input }, ctx) => {
       try {
-        // Check client
+        // Check if the client exists
         let client = await Client.findById(id);
         if (!client) throw new Error("Client not found!");
-        // Ckeck is my
+        // Check if the client is mine
         if (client.vendor.toString() !== ctx.user.id)
           throw new Error("Not your client.");
-        // Save product update
+        // Save updated product
         return await Client.findOneAndUpdate({ _id: id }, input, {
           new: true,
         });
@@ -164,18 +164,41 @@ const resolvers = {
     },
     deleteClient: async (_, { id }) => {
       try {
-        // Check product
+        // Check if the client exists
         let client = await Client.findById(id);
         if (!client) throw new Error("Product not found!");
-        // Ckeck is my
+        // Ckeck if the client is mine
         if (client.vendor.toString() !== ctx.user.id)
           throw new Error("Not your client.");
-        // Delete client
+        // Deleted client
         await Client.findOneAndDelete({ _id: id });
         return "Client delete!";
       } catch (error) {}
     },
     // ========= Orders =========
+    createOrder: async (_, { input }, ctx) => {
+      try {
+        // Destructuring
+        const { client: id } = input;
+        // Check if the client exists
+        let client = await Client.findById(id);
+        if (!client) throw new Error("Client not found!");
+        // Check if the client is mine
+        if (client.vendor.toString() !== ctx.user.id)
+          throw new Error("Not your client.");
+        // Check if stock available
+        for await (const item of input.order) {
+          const { id } = item
+          const product = await Product.findById(id)
+          if(item.stock > product.stock) {
+            throw new Error(`Product ${ product.name } exceeds quantity available.`)
+          }
+        }
+        // Assign seller
+
+        // Save in database
+      } catch (error) {}
+    },
   },
 };
 
